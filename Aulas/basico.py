1d14d7c6ebd80e6e841634735c39b4c360df5d5f
Douglas Damasio <douglasdamasio18@gmail.com>
2020-01-18 17:48:19 -0200

    Corrigindo arquivos e posicionando eles

--- Aulas/basico.py
+++ Aulas/basico.py
@@ -1,2 +1,9 @@
-user = input('Entre com seu nome: ')
-print(user)
+#! /usr/bin/python3
+# O comando acima trata-se de uma sheebang para a execução do arquivo no linux
+
+palavras = 'Palavras ao vento, são só palavras'
+
+# Toda vez que encontra a letra p é dividido
+palavras.split('p')
+
+
