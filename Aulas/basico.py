aad47ebff0540d3cc98388a72f60412cda196966
Douglas Damasio <douglasdamasio18@gmail.com>
2020-01-18 18:46:27 -0200

    Informações importantes sobre lista e exercicios

--- Aulas/basico.py
+++ Aulas/basico.py
@@ -2,8 +2,30 @@
 # O comando acima trata-se de uma sheebang para a execução do arquivo no linux
 
 palavras = 'Palavras ao vento, são só palavras'
+lista = ['jovem', 'valor', 'aula', 'comida', 'horario']
 
-# Toda vez que encontra a letra p é dividido
+# Divide de acordo com o argumento passado
 palavras.split('p')
 
+# Retorna a posição do valor argumentado em index
+lista.index('comida')
 
+# Adicionar elementos ao final da lista
+lista.append('item')
+
+# Adicionar elementos em qualquer ponto da lista (Pirmeiro elemento é a posição)
+lista.insert(0, 'Posição')
+
+# Remover itens pelo nome
+lista.remove('item')
+
+# Remove itens pela posição
+lista.pop(0)
+
+# Imprime a lista organizada
+lista.sort()
+print(lista)
+
+# Imprime a lista reversa
+lista.sort(reverse=1)
+print(lista)
