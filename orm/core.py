fed18a5094fb56dbd0e21d0fad28f143771cee4e
Douglas Damasio <douglasdamasio18@gmail.com>
2020-02-29 13:02:53 -0300

    comentarios

--- orm/core.py
+++ orm/core.py
@@ -1,9 +1,15 @@
+# Imports
 from sqlalchemy import create_engine, MetaData, Column, Table, Integer, String, DateTime
 from datetime import datetime
 
+# Cria o banco local dentro do especificado no parametro
+# Parametro echo serve para poder escrever dentro do banco 
 eng = create_engine('sqlite:///teste.db', echo=True)
+
+# Metadata apontando para a Engine
 metadata = MetaData(bind=eng)
 
+# Criação da tabela Usuários com o metadata e as colunas especificadas
 user_table = Table('usuarios', 
                     metadata,
                         Column('id', Integer, primary_key=True),
@@ -14,4 +20,11 @@ user_table = Table('usuarios',
                         Column('atualizado_em', DateTime, 
                                 default=datetime.now, onupdate=datetime.now))
 
+# primary_key - Define a chave primária
+# index - Indexação 
+# nullable - Habilitando o Not Null
+# default - Padrão
+# onupdate - Sempre que houver uma atualização
+
+# Executa tudo
 metadata.create_all(eng)
